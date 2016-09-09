b4f34dc979748f8d37adc0c98ca8e0dba8c50ee3
gaojlieMan <401633128@qq.com>
2016-09-09 20:16:05 +0800

    高结算法

--- 新闻/新闻/ViewController.m
+++ 新闻/新闻/ViewController.m
@@ -18,6 +18,12 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
+-(void)gaojiesuanfang{
+
+
+
+
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
