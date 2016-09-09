0d2f0473abae3577630068bd63abe1001ee9b32d
gaojlieMan <401633128@qq.com>
2016-09-09 20:08:10 +0800

    初始化

---
+++ 新闻/新闻/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  新闻
+//
+//  Created by gaojie on 16/9/9.
+//  Copyright © 2016年 iteam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 新闻/新闻/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  新闻
+//
+//  Created by gaojie on 16/9/9.
+//  Copyright © 2016年 iteam. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
